c12de98a3f40f60a3c973dfe959fb64b83889913
tipowers <tpowers88@outlook.com>
2021-01-31 18:47:55 -0500

    v8

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -52,6 +52,16 @@ namespace ElevenNote.WebAPI.Controllers
             return Ok();
         }
 
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
